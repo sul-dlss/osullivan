d58d3514fe5b509afedc188d5621bdb1d96d53d4
Naomi Dushay <ndushay@stanford.edu>
2017-06-29 14:51:52 -0700

    v3 annotation spec: some notes towards writing better tests

--- spec/unit/iiif/v3/presentation/annotation_spec.rb
+++ spec/unit/iiif/v3/presentation/annotation_spec.rb
@@ -4,10 +4,132 @@ describe IIIF::V3::Presentation::Annotation do
     it_behaves_like 'it has the appropriate methods for abstract_resource_only_keys v3'
   end
 
+<<<<<<< HEAD
   describe '#validate' do
     it 'raises an error if time_mode isn\'t an allowable value' do
       subject['time_mode'] = 'foo'
       expect { subject.validate }.to raise_error IIIF::V3::Presentation::IllegalValueError
     end
   end
+=======
+  # are we producing syntactically correct annotations?
+
+  # required:
+  #   id (URI?)
+  #   type "Annotation"
+  #   motivation  ('painting' unless ...)
+  #   target (URI?)
+
+  # should
+  #   body
+  #      have a test when there isn't a body;  have a test when there is a body
+  #   if there is a body:
+  #      id, type, format   (but Choice ...)
+  #      including commonly label, description, metadata, rights and attribution
+
+  # the format " must be the media type that is returned when the resource is dereferenced"
+  # the content resources may also have any of the other fields defined in this specification, including commonly label, description, metadata, rights and attribution.
+
+
+  # if it has a resource, it should have ?
+    #  id
+    #  type
+    #  format
+
+  # it MUST have a "target" (SHOULD be identified by HTTP URIs)
+  # it should have a "body" (SHOULD be identified by HTTP URIs)
+
+
+#examples:
+{
+   "id":"http://tomcrane.github.io/scratch/manifests/3/3d/anno1",
+   "type":"Annotation",
+   "motivation":"painting",
+   "body":{
+      "id":"http://files.universalviewer.io/manifests/nelis/animal-skull/animal-skull.json",
+      "type":"PhysicalObject",
+      "format":"application/vnd.threejs+json",
+      "label":"Animal Skull"
+   },
+   "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/3d"
+}
+
+
+{
+"id":"http://tomcrane.github.io/scratch/annos/3/anno/1",
+  "type":"Annotation",
+  "motivation":"painting",
+  "body":{
+      "type":"Choice",
+      "choiceHint":"client",
+      "items":[
+          {
+              "id":"http://example.org/foo.mp4",
+              "type":"Video",
+              "format":"video/mp4; codec..xxxxx"
+          },
+          {
+              "id":"http://example.org/foo.webm",
+              "type":"Video",
+              "format":"video/webm"
+          }
+      ]
+  },
+  "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/1"
+}
+
+
+{
+    "id":"...",
+    "type":"Annotation",
+    "motivation":"painting",
+    "body":{
+       "id":"http://example.org/iiif/foo2.mp3",
+       "type":"Audio"
+    },
+    "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/2"
+ }
+
+ {
+     "type" : "Annotation",
+     "motivation" : "painting",
+     "target" : "https://purl.stanford.edu/vv985tj8656/iiif/canvas/vv985tj8656_1",
+     "body" : {
+        "id" : "https://stacks.stanford.edu/image/iiif/vv985tj8656%2Fvv985tj8656_00_0001/full/full/0/default.jpg",
+        "type" : "Image",
+        "format" : "image/jpeg",
+        "width" : 9260,
+        "height" : 7412,
+        "service" : {
+           "@id" : "https://stacks.stanford.edu/image/iiif/vv985tj8656%2Fvv985tj8656_00_0001",
+           "@context" : "http://iiif.io/api/image/2/context.json",
+           "profile" : "http://iiif.io/api/image/2/level1.json"
+        }
+     },
+     "id" : "https://purl.stanford.edu/vv985tj8656/iiif/annotation/vv985tj8656_1"
+  }
+  {
+      "id":"http://tomcrane.github.io/scratch/manifests/3/pdf/anno1",
+      "type":"Annotation",
+      "motivation":"painting",
+      "body":{
+         "id":"http://example.org/iiif/some-document.pdf",
+         "type":"Document",
+         "format":"application/pdf"
+      },
+      "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/99"
+   },
+   {
+      "id":"http://tomcrane.github.io/scratch/manifests/3/pdf/anno2",
+      "type":"Annotation",
+      "motivation":"painting",
+      "body":{
+         "id":"http://example.org/iiif/cover-image.jpg",
+         "type":"Image",
+         "format":"image/jpg"
+      },
+      "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/99"
+}
+
+>>>>>>> v3 annotation spec: some notes towards writing better tests
 end
