ccb43ef89ea2f09952c0575a5c84d30fdbf7c182
Naomi Dushay <ndushay@stanford.edu>
2017-06-30 13:45:57 -0700

    v3 service_spec added to

--- spec/unit/iiif/v3/service_spec.rb
+++ spec/unit/iiif/v3/service_spec.rb
@@ -27,11 +27,92 @@ describe IIIF::V3::Service do
 
   describe '#validate' do
     # TODO:
+    #  see http://prezi3.iiif.io/api/annex/services
     # * check for required keys
+    # @context is required
+    # Image service:   The service must have the @context, @id and profile keys
+    # GeoJSON:   @context http://geojson.org/contexts/geojson-base.jsonld should be used
+    # Physical Dimension
+
     # * type check Array-only values
     # * type check String-only values
     # * type check Integer-only values
     # * type check AbstractResource-only values
+
+    it 'raises an error if @context is missing' do
+      expect { subject.validate }.to raise_error IIIF::V3::Presentation::MissingRequiredKeyError
+    end
   end
+
+
+  describe '#initialize' do
+    it 'assigns hash values passed in' do
+      image_context_uri = 'http://iiif.io/api/image/2/context.json'
+      id_uri = "https://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette"
+      profile_uri = "http://iiif.io/api/image/2/level1.json"
+      service_obj = IIIF::V3::Service.new({
+        '@context' => image_context_uri,
+        'id' => id_uri,
+        'profile' => profile_uri
+      })
+      expect(service_obj.keys.size).to eq 3
+      expect(service_obj['id']).to eq id_uri
+      expect(service_obj['@context']).to eq image_context_uri
+      expect(service_obj['profile']).to eq profile_uri
+    end
+    it 'works with "@id" as a key' do
+      id_uri = "https://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette"
+      service_obj = IIIF::V3::Service.new({
+        '@id' => id_uri,
+      })
+      expect(service_obj.keys.size).to eq 1
+      expect(service_obj['@id']).to eq id_uri
+    end
+  end
+
+  # from bc592pz8308 v3 manifest
+#   {
+#         "@context": "http://iiif.io/api/image/2/context.json",
+#         "@id": "https://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette",
+#         "id": "https://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette",
+#         "profile": "http://iiif.io/api/image/2/level1.json"
+#       }
+#       "service": {
+#          "@context": "http://iiif.io/api/image/2/context.json",
+#          "@id": "https://stacks.stanford.edu/image/iiif/bc592pz8308%2Fbc592pz8308_05_0001",
+#          "id": "https://stacks.stanford.edu/image/iiif/bc592pz8308%2Fbc592pz8308_05_0001",
+#          "profile": "http://iiif.io/api/image/2/level1.json"
+#        }
+# }
+#
+
+# bigger example
+# {
+#   "service": {
+#     "@context" : "http://iiif.io/api/image/2/context.json",
+#     "@id" : "http://www.example.org/image-service/abcd1234",
+#     "protocol": "http://iiif.io/api/image",
+#     "width" : 6000,
+#     "height" : 4000,
+#     "sizes" : [
+#       {"width" : 150, "height" : 100},
+#       {"width" : 600, "height" : 400},
+#       {"width" : 3000, "height": 2000}
+#     ],
+#     "tiles": [
+#       {"width" : 512, "scaleFactors" : [1,2,4,8,16]}
+#     ],
+#     "profile" : [
+#       "http://iiif.io/api/image/2/level2.json",
+#       {
+#         "formats" : [ "gif", "pdf" ],
+#         "qualities" : [ "color", "gray" ],
+#         "supports" : [
+#             "canonicalLinkHeader", "rotationArbitrary", "http://example.com/feature"
+#         ]
+#       }
+#     ]
+#   }
+# }
 
 end
