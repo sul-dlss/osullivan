00ff0437fe3e92fd554b01779558ee1a15cfcd8c
Naomi Dushay <ndushay@stanford.edu>
2017-06-30 13:44:31 -0700

    v3 anno spec comment out example json

--- spec/unit/iiif/v3/presentation/annotation_spec.rb
+++ spec/unit/iiif/v3/presentation/annotation_spec.rb
@@ -39,94 +39,94 @@ describe IIIF::V3::Presentation::Annotation do
 
 
 #examples:
-{
-   "id":"http://tomcrane.github.io/scratch/manifests/3/3d/anno1",
-   "type":"Annotation",
-   "motivation":"painting",
-   "body":{
-      "id":"http://files.universalviewer.io/manifests/nelis/animal-skull/animal-skull.json",
-      "type":"PhysicalObject",
-      "format":"application/vnd.threejs+json",
-      "label":"Animal Skull"
-   },
-   "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/3d"
-}
-
-
-{
-"id":"http://tomcrane.github.io/scratch/annos/3/anno/1",
-  "type":"Annotation",
-  "motivation":"painting",
-  "body":{
-      "type":"Choice",
-      "choiceHint":"client",
-      "items":[
-          {
-              "id":"http://example.org/foo.mp4",
-              "type":"Video",
-              "format":"video/mp4; codec..xxxxx"
-          },
-          {
-              "id":"http://example.org/foo.webm",
-              "type":"Video",
-              "format":"video/webm"
-          }
-      ]
-  },
-  "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/1"
-}
-
-
-{
-    "id":"...",
-    "type":"Annotation",
-    "motivation":"painting",
-    "body":{
-       "id":"http://example.org/iiif/foo2.mp3",
-       "type":"Audio"
-    },
-    "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/2"
- }
-
- {
-     "type" : "Annotation",
-     "motivation" : "painting",
-     "target" : "https://purl.stanford.edu/vv985tj8656/iiif/canvas/vv985tj8656_1",
-     "body" : {
-        "id" : "https://stacks.stanford.edu/image/iiif/vv985tj8656%2Fvv985tj8656_00_0001/full/full/0/default.jpg",
-        "type" : "Image",
-        "format" : "image/jpeg",
-        "width" : 9260,
-        "height" : 7412,
-        "service" : {
-           "@id" : "https://stacks.stanford.edu/image/iiif/vv985tj8656%2Fvv985tj8656_00_0001",
-           "@context" : "http://iiif.io/api/image/2/context.json",
-           "profile" : "http://iiif.io/api/image/2/level1.json"
-        }
-     },
-     "id" : "https://purl.stanford.edu/vv985tj8656/iiif/annotation/vv985tj8656_1"
-  }
-  {
-      "id":"http://tomcrane.github.io/scratch/manifests/3/pdf/anno1",
-      "type":"Annotation",
-      "motivation":"painting",
-      "body":{
-         "id":"http://example.org/iiif/some-document.pdf",
-         "type":"Document",
-         "format":"application/pdf"
-      },
-      "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/99"
-   },
-   {
-      "id":"http://tomcrane.github.io/scratch/manifests/3/pdf/anno2",
-      "type":"Annotation",
-      "motivation":"painting",
-      "body":{
-         "id":"http://example.org/iiif/cover-image.jpg",
-         "type":"Image",
-         "format":"image/jpg"
-      },
-      "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/99"
-}
+# {
+#    "id":"http://tomcrane.github.io/scratch/manifests/3/3d/anno1",
+#    "type":"Annotation",
+#    "motivation":"painting",
+#    "body":{
+#       "id":"http://files.universalviewer.io/manifests/nelis/animal-skull/animal-skull.json",
+#       "type":"PhysicalObject",
+#       "format":"application/vnd.threejs+json",
+#       "label":"Animal Skull"
+#    },
+#    "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/3d"
+# }
+#
+#
+# {
+# "id":"http://tomcrane.github.io/scratch/annos/3/anno/1",
+#   "type":"Annotation",
+#   "motivation":"painting",
+#   "body":{
+#       "type":"Choice",
+#       "choiceHint":"client",
+#       "items":[
+#           {
+#               "id":"http://example.org/foo.mp4",
+#               "type":"Video",
+#               "format":"video/mp4; codec..xxxxx"
+#           },
+#           {
+#               "id":"http://example.org/foo.webm",
+#               "type":"Video",
+#               "format":"video/webm"
+#           }
+#       ]
+#   },
+#   "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/1"
+# }
+#
+#
+# {
+#     "id":"...",
+#     "type":"Annotation",
+#     "motivation":"painting",
+#     "body":{
+#        "id":"http://example.org/iiif/foo2.mp3",
+#        "type":"Audio"
+#     },
+#     "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/2"
+#  }
+#
+#  {
+#      "type" : "Annotation",
+#      "motivation" : "painting",
+#      "target" : "https://purl.stanford.edu/vv985tj8656/iiif/canvas/vv985tj8656_1",
+#      "body" : {
+#         "id" : "https://stacks.stanford.edu/image/iiif/vv985tj8656%2Fvv985tj8656_00_0001/full/full/0/default.jpg",
+#         "type" : "Image",
+#         "format" : "image/jpeg",
+#         "width" : 9260,
+#         "height" : 7412,
+#         "service" : {
+#            "@id" : "https://stacks.stanford.edu/image/iiif/vv985tj8656%2Fvv985tj8656_00_0001",
+#            "@context" : "http://iiif.io/api/image/2/context.json",
+#            "profile" : "http://iiif.io/api/image/2/level1.json"
+#         }
+#      },
+#      "id" : "https://purl.stanford.edu/vv985tj8656/iiif/annotation/vv985tj8656_1"
+#   }
+#   {
+#       "id":"http://tomcrane.github.io/scratch/manifests/3/pdf/anno1",
+#       "type":"Annotation",
+#       "motivation":"painting",
+#       "body":{
+#          "id":"http://example.org/iiif/some-document.pdf",
+#          "type":"Document",
+#          "format":"application/pdf"
+#       },
+#       "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/99"
+#    },
+#    {
+#       "id":"http://tomcrane.github.io/scratch/manifests/3/pdf/anno2",
+#       "type":"Annotation",
+#       "motivation":"painting",
+#       "body":{
+#          "id":"http://example.org/iiif/cover-image.jpg",
+#          "type":"Image",
+#          "format":"image/jpg"
+#       },
+#       "target":"http://tomcrane.github.io/scratch/manifests/3/canvas/99"
+# }
 
 end
